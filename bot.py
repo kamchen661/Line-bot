e89893d84bf6fb3a8efc5c3893ce0656dd37f6c2
kamchen661 <63591431+kamchen661@users.noreply.github.com>
2020-04-14 21:14:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ clMID = cl.profile.mid
 myProfile["displayName"] = clProfile.displayName
 myProfile["statusMessage"] = clProfile.statusMessage
 myProfile["pictureStatus"] = clProfile.pictureStatus
-admin=['ud710f342e9915bf19d1bab0185e58152','uc3785b8c9e23fc5e9eb0ef9311c1f430'］
+admin=['ud710f342e9915bf19d1bab0185e58152','uc3785b8c9e23fc5e9eb0ef9311c1f430','clMID］
 msg_dict = {}
 wait2 = {
     'readPoint':{},
